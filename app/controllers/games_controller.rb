8eb61e9e5589386fec8658ca2bdfa97a2091f857
Stevenson Davis Stevensondavis1@gmail.com <stevensondavis1@gmail.com>
2025-11-10 20:47:37 +0900

    word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,20 @@
+class GamesController < ApplicationController
+end
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @word = params[:word]
+    @letters = params[:letters]
+  end
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @word = params[:word]
+    @letters = params[:letters]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "games#new"
+  get "/new", to: "games#new", as: :new
+  get "/score", to: "games#score", as: :score
 end
